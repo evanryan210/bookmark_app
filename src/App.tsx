2922d853fe837929aa26dfc154011aa4c37959e4
evanryan210 <evanryan99@gmail.com>
2022-12-14 16:38:01 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import * as styles from './styles'
+
+//open graph protocol is the tool used to preview links
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [bookmark, setBookmark] = useState('')
+  const [bookmarks, setBookmarks] = useState<string[]>([])
+
+  const addBookmark = () => {
+    setBookmarks((existingBookmarks) => [...existingBookmarks, bookmark]);
+    setBookmark('');
+  }
+  const removeBookmark = (index: number) => {
+    bookmarks.splice(index, index + 1);
+    setBookmarks((existingBookmarks) => [...bookmarks]);
+}
+
+  const enterInputHandler = (event: any) => {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      addBookmark();
+    }
+  }
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.addEventListener("keypress", enterInputHandler);
+    }
+    return () => {
+      inputRef.current?.removeEventListener("keypress", enterInputHandler)
+    }
+  }, [addBookmark])
+
+
+  console.log(bookmarks)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bookmark App Thing!</h1>
+
+      <label htmlFor='linkInput'>URL:</label>
+      <input ref={inputRef} id='linkInput' value={bookmark} onInput={(ev) => {
+        setBookmark(ev.currentTarget.value)
+      }}></input>
+      <button onClick={addBookmark}>Add Bookmark</button>
+
+      {bookmarks.map((bookmark, index) => {
+        return (
+          <div>
+            <p>{bookmark}</p>
+            <button onClick={() => {
+              removeBookmark(index);
+            }}>Delete</button>
+          </div>
+        )
+      })}
     </div>
   );
 }
---
+++ src/styles.ts
@@ -0,0 +1,6 @@
+import { mergeStyles } from "@fluentui/merge-styles";
+
+
+export const header = mergeStyles({
+    
+})
